24338748bc939df51ef8a556bb2cdde3ecb6d13d
koral <koral@mailoo.org>
2017-09-06 23:51:30 +0200

    Add lenses to access extensions

--- Text/RSS/Lens.hs
+++ Text/RSS/Lens.hs
@@ -1,11 +1,14 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell  #-}
 module Text.RSS.Lens (module Text.RSS.Lens) where
 
 -- {{{ Imports
-import           Lens.Simple
-
 import           Text.RSS.Types
 
+import           Data.Singletons.Prelude
+import           Data.Vinyl.Lens
+import           Data.Vinyl.TypeLevel
+import           Lens.Simple
 import           URI.ByteString
 -- }}}
 
@@ -29,6 +32,11 @@ itemEnclosureL :: Traversal' (RssItem e) RssEnclosure
 itemEnclosureL inj a@RssItem { itemEnclosure = e } = (\x -> a { itemEnclosure = e }) <$> traverse inj e
 {-# INLINE itemEnclosureL #-}
 
+itemExtensionL :: SingI a => RElem a e (RIndex a e) => Lens' (RssItem e) (RssItemExtension a)
+itemExtensionL = itemExtensionsL . f . rlens sing where
+  f inj (RssItemExtensions a) = RssItemExtensions <$> inj a
+{-# INLINE itemExtensionL #-}
+
 $(makeLensesBy (\n -> Just (n ++ "L")) ''RssTextInput)
 $(makeLensesBy (\n -> Just (n ++ "L")) ''RssCloud)
 $(makeLensesBy (\n -> Just (n ++ "L")) ''RssImage)
@@ -46,3 +54,8 @@ channelItemsL inj a@RssDocument { channelItems = i } = (\x -> a { channelItems =
 channelCategoriesL :: Traversal' (RssDocument e) RssCategory
 channelCategoriesL inj a@RssDocument { channelCategories = c } = (\x -> a { channelCategories = c }) <$> traverse inj c
 {-# INLINE channelCategoriesL #-}
+
+channelExtensionL :: SingI a => RElem a e (RIndex a e) => Lens' (RssDocument e) (RssChannelExtension a)
+channelExtensionL = channelExtensionsL . f . rlens sing where
+  f inj (RssChannelExtensions a) = RssChannelExtensions <$> inj a
+{-# INLINE channelExtensionL #-}
